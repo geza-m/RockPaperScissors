53ee083f3f62a2384ab2fd3bcb4c4cdd261271d9
geza-m <110627477+geza-m@users.noreply.github.com>
2023-01-26 00:00:37 +0100

    first working version.

--- script.js
+++ script.js
@@ -1,53 +1,87 @@
+
+
+
 function getComputerChoice() {
     //random number between 0,1 or 2
      const choice = Math.floor(Math.random()*3);
   
     //returns Rock, Paper or Scissors
-     return (choice==0) ? "Rock" : (choice==1) ? "Paper" : "Scissors";
+     return (choice==0) ? "rock" : (choice==1) ? "paper" : "scissors";
   }
   
-function gameRound (playerSelection, computerSelection) {
-let message =""
-winningVariations = ["rockscissor", "scissorspaper", "paperrock"];
-if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-    message = "Draw.";
-}
-else if (winningVariations.includes(playerSelection.toLowerCase()+computerSelection.toLowerCase()))  {
-    message = `You Win! ${playerSelection} beats ${computerSelection}.`;
-}
-else {
-    message =   `You Lose! ${computerSelection} beats ${playerSelection}.`;
-}
-return message;
+function playRound (playerSelection, computerSelection) {
+  let message =""
+  let result=0;
+  let computerSelectionCap = computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1);
+  let playerSelectionCap = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
+  winningVariations = ["rockscissor", "scissorspaper", "paperrock"];
+  if (playerSelection === computerSelection) {
+      message = "Draw.";
+  }
+  else if (winningVariations.includes(playerSelection+computerSelection))  {
+    
+      message = `You Win! ${playerSelectionCap} beats ${computerSelectionCap}.`;
+      result = 1;
+  }
+  else {
+      message =   `You Lose! ${computerSelectionCap} beats ${playerSelectionCap}.`;
+      result = 2;
+  }
+
+  return { result:result, message: message};
+  return message;
 
 }
     
-numberOfRounds = 5;
 
 
+function game() {
 
-
-function startGame() {
-    let input = document.getElementById("myInput");
-    input.addEventListener("input", function(){
-        let inputValue = input.value;
-        console.log(typeof inputValue, inputValue);
-
-
-        let result = gameRound (inputValue, getComputerChoice());
-
-        const newDiv = document.createElement("div");
-        newDiv.textContent = result;
+  let playerPoints = 0;
+  let computerPoints =0;
+  for (let i = 0; i < 5;i++) {
+    let weapon = "";
+    while (weapon!=="rock" && weapon!=="paper" && weapon!== "scissors")
+    {
+      weapon = prompt(`Round ${i+1}\nRock Paper or Scissors`).toLowerCase();
+    }
     
-        const myElement= document.getElementById("results");
-        myElement.appendChild(newDiv);
-    });
+    let computerChoice = getComputerChoice();
+    let result = playRound (weapon, computerChoice);
+    let resultMessage = result.message;
     
+    switch (result.result) {
+      case 1:
+        playerPoints++;
+        break;
+      case 2:
+        computerPoints++;
+        break
+    }
 
-}
-
-window.onload = (event) => {
-    startGame();
-};
+    let weaponCap = weapon.charAt(0).toUpperCase()+weapon.slice(1);
+    let computerCap =  computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1);
+    console.log(`You: ${weaponCap} | Computer: ${computerCap}\n`+resultMessage);
+  }
   
+  if (playerPoints > computerPoints) {
+    console.log("PLAYER WON!");
+  }
+  else if (playerPoints < computerPoints) {
+    console.log("COMPUTER WON!");
+  }
+  else {
+    console.log("DRAW.");
+  }
+}
+
+
+let el = document.querySelector("#startButton");
+el.addEventListener("click", function(e) {
+game();
+});
   
+
+
+
+
